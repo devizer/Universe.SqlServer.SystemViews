79a6cdb8aca5758be53b771a3433671cff049082
devizer <devizer@github.io>
2025-11-15 21:28:59 +0200

    2nd Standalone Build moced to folder BUILD-TOOL-And-STANDALONE-CLI

--- Universe.SqlServer.AdministrativeViews.CLI/BUILD-TOOL-And-STANDALONE-CLI/Build-CLI-Tool.cmd
+++ Universe.SqlServer.AdministrativeViews.CLI/BUILD-TOOL-And-STANDALONE-CLI/Build-CLI-Tool.cmd
@@ -1,7 +1,9 @@
 rem 1) NEEDS C:\Apps\dotnet-10\dotnet.exe
 rem 2) NEEDS "C:\Program Files\Git\bin\bash.exe"
 
+pushd ..
 "C:\Program Files\Git\bin\bash.exe" -c "export LC_ALL=en_US.UTF-8; Reset-Target-Framework -fw 'net6.0;net8.0;net10.0'"
 rd /q /s bin
-powershell -f "how to pack.ps1"
+powershell -f "BUILD-TOOL-And-STANDALONE-CLI\how to pack.ps1"
 "C:\Program Files\Git\bin\bash.exe" -c "export LC_ALL=en_US.UTF-8; Reset-Target-Framework -fw 'net6.0;net35;net40;net45;net46;net462;net48;net8.0'"
+popd
