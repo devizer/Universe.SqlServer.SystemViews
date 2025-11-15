5adcae6ecd4c9f8eecf330af6f3200beabbffe9f
devizer <devizer@github.io>
2025-11-16 00:49:34 +0200

    11th Standalone Build moved to folder BUILD-TOOL-And-STANDALONE-CLI

--- Universe.SqlServer.AdministrativeViews.CLI/BUILD-TOOL-And-STANDALONE-CLI/3) Create-GitHub-Release.cmd
+++ Universe.SqlServer.AdministrativeViews.CLI/BUILD-TOOL-And-STANDALONE-CLI/3) Create-GitHub-Release.cmd
@@ -2,12 +2,14 @@ call Set-Environment-Variables.cmd
 if Not Defined BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS Goto :Err
 for /f %%i in (%BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\PUBLIC\VERSION.TXT) do set VERSION=%%i
 
-echo VERSION IS %VERSION%
-(
-  echo SqlServer Administrative Views standalone release v%VERSION%
-  echo.
-  echo [SqlServer Administrative Views .NET tool]^(https://www.nuget.org/packages/SqlServer.AdministrativeViews^) is also available
-) | gh release create -t "v%VERSION%" -F - "v%VERSION%" %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\PUBLIC\*.*
+echo SqlServer Administrative Views standalone release v%VERSION% > %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
+echo.  >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
+echo [SqlServer Administrative Views .NET tool]^(https://www.nuget.org/packages/SqlServer.AdministrativeViews^) is also available: >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
+echo ``` >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
+echo dotnet tool install --global SqlServer.AdministrativeViews >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
+echo ``` >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
+
+gh release create -t "v%VERSION%" -F %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt "v%VERSION%" %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\PUBLIC\*.*
 if ERRORLEVEL 1 Goto :err
 echo Completed: Release v%VERSION%
 
